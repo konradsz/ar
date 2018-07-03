335cdd4e0529ce1525982b7260d0b01d65f3c525
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2018-07-03 21:10:08 +0200

    handle key presses

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <vector>
+
+
+void calibrate_camera(const std::vector<cv::Mat>& /*images*/) {
+//    cv::Mat camera_matrix;
+}
 
 int main() {
     cv::VideoCapture capture = cv::VideoCapture(0);
@@ -13,27 +19,51 @@ int main() {
 
     cv::namedWindow("ar", cv::WINDOW_AUTOSIZE);
 
-    while (true) {
-        // capture the image from camera
+    std::vector<cv::Mat> calibration_images;
+
+    bool finished = false;
+    while (!finished) {
         cv::Mat image;
         capture >> image;
 
-        // flip the image
         cv::Mat flipped_image;
         cv::flip(image, flipped_image, 1);
 
-        // convert to grey scale
-        cv::Mat gray_image;
-        cv::cvtColor(flipped_image, gray_image, cv::COLOR_BGR2GRAY);
-
         cv::Size pattern_size(9, 6);
         std::vector<cv::Point2f> centers;
-        //cv::resize(gray_image, gray_image, cv::Size(320, 240));
-        bool found = cv::findChessboardCorners(gray_image, pattern_size, centers);
-        cv::drawChessboardCorners(flipped_image, pattern_size, cv::Mat(centers), found);
+        bool found = cv::findChessboardCorners(
+            flipped_image, pattern_size, centers,
+            cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_NORMALIZE_IMAGE | cv::CALIB_CB_FAST_CHECK);
 
-        cv::imshow("ar", flipped_image);
-        if (cv::waitKey(1) == 27) {
+        if (found) {
+            cv::Mat image_with_chessboard;
+            flipped_image.copyTo(image_with_chessboard);
+            cv::drawChessboardCorners(image_with_chessboard, pattern_size, cv::Mat(centers), found);
+            cv::imshow("ar", image_with_chessboard);
+        } else {
+            cv::imshow("ar", flipped_image);
+        }
+
+        char key = cv::waitKey(10);
+        switch (key) { // ESC
+        case ' ':
+            if (found) {
+                calibration_images.push_back(flipped_image);
+                std::cout << "Calibration image " << calibration_images.size() << " collected\n";
+            } else {
+                std::cout << "Chessboard on image not found\n";
+            }
+            break;
+        case 'c':
+            if (calibration_images.size() >= 15) {
+                calibrate_camera(calibration_images);
+            } else {
+                std::cout << "Not enough of calibration images (" << calibration_images.size()
+                          << " < 15)\n";
+            }
+            break;
+        case 27:
+            finished = true;
             break;
         }
     }
