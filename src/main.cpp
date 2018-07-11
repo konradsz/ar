b1211f0f3935e826d4f358b05a296d393c1238eb
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2018-07-11 20:55:43 +0200

    change variables names, clean up code

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ bool read_calibration_file(const std::string& file_name, cv::Mat& camera_matrix,
                            cv::Mat& distortion_coefficients) {
     cv::FileStorage camera_calibration_file(file_name, cv::FileStorage::READ);
     if (!camera_calibration_file.isOpened()) {
-        std::cerr << "Cannot open calibration file: " << file_name << "\n";
+        std::cerr << "Cannot open calibration file: " << file_name << '\n';
         return false;
     }
 
@@ -34,14 +34,21 @@ bool read_calibration_file(const std::string& file_name, cv::Mat& camera_matrix,
     return true;
 }
 
-void calcChessboardCorners(cv::Size boardSize, float squareSize,
+void calcChessboardCorners(const cv::Size& board_size, float square_size,
                            std::vector<cv::Point3f>& corners) {
-    for (int i = 0; i < boardSize.height; i++)
-        for (int j = 0; j < boardSize.width; j++)
-            corners.push_back(cv::Point3f(float(j * squareSize), float(i * squareSize), 0));
+    for (int i = 0; i < board_size.height; ++i) {
+        for (int j = 0; j < board_size.width; ++j) {
+            corners.push_back(cv::Point3f(float(j * square_size), float(i * square_size), 0));
+        }
+    }
 }
 
 int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "No calibration file name passed!" << '\n';
+        return -1;
+    }
+
     const auto FRAME_WIDTH = 640;
     const auto FRAME_HEIGHT = 480;
 
@@ -49,37 +56,31 @@ int main(int argc, char* argv[]) {
     capture.set(cv::CAP_PROP_FRAME_WIDTH, FRAME_WIDTH);
     capture.set(cv::CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT);
 
-    cv::Mat camera_matrix = cv::Mat::eye(3, 3, CV_64F);
-    cv::Mat distortion_coefficients = cv::Mat::zeros(8, 1, CV_64F);
-
-    bool params_file_read = false;
-    if (argc == 2) {
-        params_file_read = read_calibration_file(argv[1], camera_matrix, distortion_coefficients);
-    }
-
     if (!capture.isOpened()) {
         std::cerr << "Cannot use the camera!\n";
         return -1;
     }
 
+    cv::Mat camera_matrix = cv::Mat::eye(3, 3, CV_64F);
+    cv::Mat distortion_coefficients = cv::Mat::zeros(8, 1, CV_64F);
+
+    const std::string file_name = argv[1];
+    bool calibrated = read_calibration_file(file_name, camera_matrix, distortion_coefficients);
+
     cv::namedWindow("ar", cv::WINDOW_AUTOSIZE);
 
-    std::vector<cv::Mat> calibration_images;
-    std::vector<std::vector<cv::Point2f>> calibration_corners; // image_space_corners
-    std::vector<std::vector<cv::Point3f>> known_corners;
-
-    bool calibrated = false;
+    std::vector<std::vector<cv::Point2f>> calibration_corners;
 
     bool finished = false;
     double tick_previous = cv::getTickCount();
     double tick_current = cv::getTickCount();
     while (!finished) {
         tick_current = cv::getTickCount();
-        cv::Mat image;
-        capture >> image;
+        cv::Mat frame;
+        capture >> frame;
 
-        cv::Mat flipped_image;
-        cv::flip(image, flipped_image, 1);
+        cv::Mat flipped_frame;
+        cv::flip(frame, flipped_frame, 1);
 
         const int corners_horizontally = 9;
         const int corners_vertically = 6;
@@ -90,75 +91,63 @@ int main(int argc, char* argv[]) {
 
         bool found = false;
         if (t > 2.0) {
-            found = cv::findChessboardCorners(flipped_image, pattern_size, corners,
+            found = cv::findChessboardCorners(flipped_frame, pattern_size, corners,
                                               cv::CALIB_CB_ADAPTIVE_THRESH |
                                                   cv::CALIB_CB_NORMALIZE_IMAGE |
                                                   cv::CALIB_CB_FAST_CHECK);
         }
 
-        std::vector<cv::Point3f> world_space_corners;
-        for (auto i = 0; i < corners_horizontally; ++i) {
-            for (auto j = 0; j < corners_vertically; ++j) {
-                world_space_corners.push_back(cv::Point3f(i, j, 0.0f));
-            }
-        }
-
         if (found && !calibrated) {
-            cv::Mat image_with_chessboard;
-            flipped_image.copyTo(image_with_chessboard);
+            cv::Mat frame_with_chessboard;
+            flipped_frame.copyTo(frame_with_chessboard);
 
-            cv::Mat gray_image;
-            cv::cvtColor(flipped_image, gray_image, cv::COLOR_RGB2GRAY);
+            cv::Mat gray_frame;
+            cv::cvtColor(flipped_frame, gray_frame, cv::COLOR_RGB2GRAY);
             cv::cornerSubPix(
-                gray_image, corners, cv::Size(11, 11), cv::Size(-1, -1),
+                gray_frame, corners, cv::Size(5, 5), cv::Size(-1, -1),
                 cv::TermCriteria(cv::TermCriteria::EPS | cv::TermCriteria::COUNT, 30, 0.1));
-            cv::drawChessboardCorners(image_with_chessboard, pattern_size, cv::Mat(corners), found);
-            cv::imshow("ar", image_with_chessboard);
+            cv::drawChessboardCorners(frame_with_chessboard, pattern_size, cv::Mat(corners), found);
+            cv::imshow("ar", frame_with_chessboard);
 
-            calibration_images.push_back(flipped_image);
             calibration_corners.push_back(corners);
-            known_corners.push_back(world_space_corners);
-            std::cout << "Calibration image " << calibration_images.size() << " collected\n";
+            std::cout << "Calibration corners " << calibration_corners.size() << " collected\n";
             tick_previous = cv::getTickCount();
-        } else if (calibrated || params_file_read) {
-            cv::Mat imageUndistorted;
-            cv::undistort(flipped_image, imageUndistorted, camera_matrix, distortion_coefficients);
-            cv::imshow("ar", imageUndistorted);
+        } else if (calibrated) {
+            cv::Mat undistorted_frame;
+            cv::undistort(flipped_frame, undistorted_frame, camera_matrix, distortion_coefficients);
+            cv::imshow("ar", undistorted_frame);
         } else {
-            cv::imshow("ar", flipped_image);
+            cv::imshow("ar", flipped_frame);
         }
 
-        const int MIN_NUMBER_OF_IMAGES = 3;
+        const int MIN_NUMBER_OF_SAMPLES = 15;
 
         char key = cv::waitKey(10);
         switch (key) {
         case ' ': // call try_store_corners()
             if (found) {
-                calibration_images.push_back(flipped_image);
                 calibration_corners.push_back(corners);
-                known_corners.push_back(world_space_corners);
-                std::cout << "Calibration image " << calibration_images.size() << " collected\n";
+                std::cout << "Calibration corners " << calibration_corners.size() << " collected\n";
             } else {
                 std::cout << "Chessboard on image not found\n";
             }
             break;
         case 'c': // call calibrate()
         {
-            if (calibration_images.size() >= MIN_NUMBER_OF_IMAGES) {
-
-                std::vector<cv::Mat> rvecs;
-                std::vector<cv::Mat> tvecs;
+            if (calibration_corners.size() >= MIN_NUMBER_OF_SAMPLES) {
+                std::vector<cv::Mat> rotation_vectors;
+                std::vector<cv::Mat> translation_vectors;
 
                 camera_matrix.at<double>(0, 0) = 1;
 
-                std::vector<std::vector<cv::Point3f>> objectPoints(1);
-                calcChessboardCorners(cv::Size(9, 6), 1.0, objectPoints[0]);
-                objectPoints.resize(calibration_corners.size(), objectPoints[0]);
+                std::vector<std::vector<cv::Point3f>> object_points(1);
+                calcChessboardCorners(pattern_size, 1.0, object_points[0]);
+                object_points.resize(calibration_corners.size(), object_points[0]);
 
-                double rms =
-                    cv::calibrateCamera(objectPoints, calibration_corners, image.size(),
-                                        camera_matrix, distortion_coefficients, rvecs, tvecs);
-                std::cout << "calibration error: " << rms << "\n";
+                double rms = cv::calibrateCamera(object_points, calibration_corners, frame.size(),
+                                                 camera_matrix, distortion_coefficients,
+                                                 rotation_vectors, translation_vectors);
+                std::cout << "calibration error: " << rms << '\n';
                 // calibrate_camera(calibration_images, calibration_corners);
 
                 calibrated = true;
@@ -168,26 +157,27 @@ int main(int argc, char* argv[]) {
                     for (auto j = 0; j < 3; ++j) {
                         std::cout << camera_matrix.at<double>(i, j) << " ";
                     }
-                    std::cout << "\n";
+                    std::cout << '\n';
                 }
                 std::cout << "distortion_coefficients:\n";
                 for (auto i = 0; i < 8; ++i) {
                     std::cout << distortion_coefficients.at<double>(i) << " ";
                 }
-                std::cout << "\n";
+                std::cout << '\n';
             } else {
-                std::cout << "Not enough of calibration images (" << calibration_images.size()
-                          << " < " << MIN_NUMBER_OF_IMAGES << ")\n";
+                std::cout << "Not enough of samples (" << calibration_corners.size() << " < "
+                          << MIN_NUMBER_OF_SAMPLES << ")\n";
             }
 
-            cv::FileStorage camera_calibration_file("../data/camera_calibration",
-                                                    cv::FileStorage::WRITE);
+            cv::FileStorage camera_calibration_file(file_name, cv::FileStorage::WRITE);
             camera_calibration_file << "camera_matrix" << camera_matrix;
             camera_calibration_file << "distortion_coefficients" << distortion_coefficients;
             camera_calibration_file.release();
 
+            std::cout << "Camera calibration file saved: " << file_name << '\n';
             break;
         }
+        case 'q':
         case 27: // ESC
             finished = true;
             break;
